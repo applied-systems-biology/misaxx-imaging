52c2e5fdf8891ac839639eb35de3b31f83c2b442
Ruman Gerst <ruman.gerst@leibniz-hki.de>
2019-01-24 16:50:47 +0100

    + Added custom TIFF IO

---
+++ include/misaxx/imaging/utils/tiffio.h
@@ -0,0 +1,28 @@
+//
+// Created by rgerst on 24.01.19.
+//
+
+#pragma once
+
+#include <opencv2/opencv.hpp>
+#include <boost/filesystem/path.hpp>
+
+namespace misaxx::imaging::utils {
+    /**
+     * Reads a cv::Mat from TIFF. Supports all types supported by OpenCV
+     * @param t_path
+     * @return
+     */
+    extern cv::Mat imread(const boost::filesystem::path &t_path);
+
+    /**
+     * Writes a cv::Mat to TIFF. Supports all types supported by OpenCV
+     * @param t_img
+     * @param t_path
+     */
+    extern void imwrite(const cv::Mat &t_img, const boost::filesystem::path &t_path);
+}
+
+
+
+
